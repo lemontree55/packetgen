0259b565e550e461b9647d4fc8d1683563237b9b
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-11-25 19:59:07 +0100

    Add Header::Eth specs.

--- lib/packetgen/header/eth.rb
+++ lib/packetgen/header/eth.rb
@@ -20,12 +20,12 @@ module PacketGen
           unless bytes.size == 6
             raise ArgumentError, 'not a MAC address'
           end
-          self[:a0] = bytes[0]
-          self[:a1] = bytes[1]
-          self[:a2] = bytes[2]
-          self[:a3] = bytes[3]
-          self[:a4] = bytes[4]
-          self[:a5] = bytes[5]
+          self[:a0] = bytes[0].to_i(16)
+          self[:a1] = bytes[1].to_i(16)
+          self[:a2] = bytes[2].to_i(16)
+          self[:a3] = bytes[3].to_i(16)
+          self[:a4] = bytes[4].to_i(16)
+          self[:a5] = bytes[5].to_i(16)
           self
         end
 
@@ -36,12 +36,53 @@ module PacketGen
         end
       end
 
+      # @param [Hash] options
       def initialize(options={})
-        super MacAddr.parse(options[:dst]),
-              MacAddr.parse(options[:src]),
+        super MacAddr.new.parse(options[:dst] || '00:00:00:00:00:00'),
+              MacAddr.new.parse(options[:src] || '00:00:00:00:00:00'),
               Int16.new(options[:proto] || 0),
-              StructFu::String.new.read(args[:body])
+              StructFu::String.new.read(options[:body])
       end
+
+      # Get MAC destination address
+      # @return [String]
+      def dst
+        self[:dst].to_x
+      end
+
+      # Set MAC destination address
+      # @param [String] addr
+      # @return [String]
+      def dst=(addr)
+        self[:dst].parse addr
+      end
+
+      # Get MAC source address
+      # @return [String]
+      def src
+        self[:src].to_x
+      end
+
+      # Set MAC source address
+      # @param [String] addr
+      # @return [String]
+      def src=(addr)
+        self[:src].parse addr
+      end
+
+      # Get protocol field
+      # @return [Integer]
+      def proto
+        self[:proto].to_i
+      end
+
+      # Set protocol field
+      # @param [Integer] proto
+      # @return [Integer]
+      def proto=(proto)
+        self[:proto].value = proto
+      end
+      
     end
   end
 end
---
+++ spec/header/eth_spec.rb
@@ -0,0 +1,76 @@
+require_relative '../spec_helper'
+
+module PacketGen
+  module Header
+
+    describe Eth::MacAddr do
+      before(:each) do
+        @mac = Eth::MacAddr.new.parse('00:01:02:03:04:05')
+      end
+
+      it '#parse a MAC address string' do
+        expect(@mac.a0).to eq(0)
+        expect(@mac.a1).to eq(1)
+        expect(@mac.a2).to eq(2)
+        expect(@mac.a3).to eq(3)
+        expect(@mac.a4).to eq(4)
+        expect(@mac.a5).to eq(5)
+      end
+
+      it '#to_x returns a MAC address string' do
+        expect(@mac.to_x).to eq('00:01:02:03:04:05')
+      end
+    end
+
+    describe Eth do
+
+      describe '#initialize' do
+        it 'creates a Ethernet header with default values' do
+          eth = Eth.new
+          expect(eth).to be_a(Eth)
+          expect(eth.dst).to eq('00:00:00:00:00:00')
+          expect(eth.src).to eq('00:00:00:00:00:00')
+          expect(eth.proto).to eq(0)
+        end
+
+        it 'accepts options' do
+          options = {
+            dst: '00:01:02:03:04:05',
+            src: '00:ff:ff:ff:ff:4c',
+            proto: 0x800,
+            body: 'this is a body'
+          }
+          eth = Eth.new(options)
+          options.each do |key, value|
+            expect(eth.send(key)).to eq(value)
+          end
+        end
+      end
+
+      describe 'setters' do
+        before(:each) do
+          @eth = Eth.new
+        end
+
+        it '#dst= accepts a MAC address string' do
+          @eth.dst = 'ff:fe:fd:fc:fb:fa'
+          6.times do |i|
+            expect(@eth[:dst]["a#{i}".to_sym]).to eq(0xff - i)
+          end
+        end
+
+        it '#src= accepts a MAC address string' do
+          @eth.src = 'ff:fe:fd:fc:fb:fa'
+          6.times do |i|
+            expect(@eth[:src]["a#{i}".to_sym]).to eq(0xff - i)
+          end
+        end
+
+        it '#proto= accepts an integer' do
+          @eth.proto = 0xabcd
+          expect(@eth[:proto].value).to eq(0xabcd)
+        end
+      end
+    end
+  end
+end
