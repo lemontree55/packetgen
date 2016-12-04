6986b630af887f378cfd786897e281a8fac58a3a
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-04 13:30:38 +0100

    Add Capture class, and a first #start spec. Need root permissions, so not yet tested (need CI).

---
+++ lib/packetgen/capture.rb
@@ -0,0 +1,74 @@
+module PacketGen
+
+  # Capture packets from wire
+  # @author Sylvain Daubert
+  class Capture
+
+    # Default snaplen to use if :snaplen option not defined
+    DEFAULT_SNAPLEN = 0xffff
+
+    # Get captured packets
+    # @return [Array<Packets>]
+    attr_reader :packets
+
+    # Get captured packet raw data
+    # @return [Array<String>]
+    attr_reader :raw_packets
+
+    # @param [String] iface interface on which capture packets
+    # @param [Hash] options
+    # @option options [Integer] :max maximum number of packets to capture
+    # @option options [Integer] :timeout maximum number of seconds before end
+    #    of capture
+    # @option options [String] :filter bpf filter
+    # @option options [Boolean] :promiscuous (default: +false+)
+    # @option options [Integer] :snaplen maximum number of bytes to capture for
+    def initialize(iface, options={})
+      @packets = []
+      @raw_packets = []
+      @iface = iface
+      @max = options[:max]
+      @timeout = options[:timeout] || 1
+      @filter = options[:filter]
+      @promisc = options[:promisc] || false
+      @snaplen = options[:snaplen] || DEFAULT_SNAPLEN
+      @parse = options[:parse].nil? ? true : options[:parse]
+    end
+
+    # Start capture
+    # @param [Hash] options complete options from {#initialize}.
+    # @option options [Integer] :max maximum number of packets to capture
+    # @option options [String] :filter
+    # @option options [Boolean] :parse parse raw data to generate packets before
+    #    yielding.  Default: +true+
+    # @yieldparam [Packet,String] packet_data if a block is given, yield each
+    #    captured packet (Packet or raw data String, depending on +:parse+)
+    def start(options={})
+      @pcap = PCAPRUB::Pcap.open_live(@iface, @snaplen, @promisc, @timeout)
+      set_filter
+      @pcap.each do |packet_data|
+        p packet_data
+        @raw_packets << packet_data
+        if @parse
+          packet = Packet.parse(packet_data)
+          p packet
+          @packets << packet
+          yield packet
+        else
+          yield packet_data
+        end
+        if @max
+          break if @raw_packets.size >= @max
+        end
+      end
+    end
+
+    private
+
+    def set_filter
+      return if @filter.nil?
+      return if @filter.empty?
+      @pcap.set_filter @filter
+    end
+  end
+end
---
+++ spec/capture_spec.rb
@@ -0,0 +1,39 @@
+require_relative 'spec_helper'
+
+module PacketGen
+  describe Capture do
+
+    let(:iface) { PacketGen.default_iface }
+
+    describe '#initialize' do
+      it 'accepts no options' do
+        cap = nil
+        expect { cap = Capture.new(iface) }.to_not raise_error
+        expect(cap).to be_a(Capture)
+      end
+
+      it 'accepts options' do
+        options = { max: 12, timeout: 30, filter: 'ip', promisc: true, snaplen: 45 }
+        expect { Capture.new(iface, options) }.to_not raise_error
+      end
+    end
+
+    describe '#start' do
+      it 'capture packets and returns a array of Packet' do
+        cap = Capture.new('lo')
+        cap_thread = Thread.new { cap.start }
+        system 'ping 127.0.0.1 -c 3 -W 0.1 -w 1 > /dev/null'
+        cap_thread.kill
+        packets = cap.packets
+        expect(packets).to be_a(Array)
+        expect(packets.size).to eq(3)
+        expect(packets.all? { |p| p.is_a? Packet }).to be(true)
+      end
+
+      it 'capture packets until :timeout seconds'
+      it 'capture packets using a filter'
+      it 'capture packets and returns a array of string with :parse option to false'
+      it 'capture :max packets'
+    end
+  end
+end
