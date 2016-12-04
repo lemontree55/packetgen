7df482059179a7417461aa6b733a317e8a2e9d9e
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-04 20:39:23 +0100

    Move including of CaptureHelper module in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,10 +8,13 @@ RSpec.configure do |c|
   if c.filter[:sudo]
     SimpleCov.command_name 'rspec:sudo'
   end
-  c.include CaptureHelper
 end
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'packetgen'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |c|
+  c.include CaptureHelper
+end
--- spec/support/capture_helper.rb
+++ spec/support/capture_helper.rb
@@ -1,9 +1,9 @@
 module CaptureHelper
-  # capture(options) { 'ping -c 125 127.0.0.1 }
-  def capture(options={}, &blk)
+  # capture(iface, options) { 'ping -c 125 127.0.0.1 }
+  def capture(iface, options={}, &blk)
     timeout = options[:timeout] || 0
 
-    cap = Capture.new('lo', options)
+    cap = Capture.new(iface, options)
     cap_thread = Thread.new { cap.start }
     sleep 0.1
     blk.call
