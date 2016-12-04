40af48b59a7721503167fb4d8d8a3bb778d70677
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-04 18:47:17 +0100

    simplecov: add a different command name for spec:sudo. thus, SimpleCov can merge normal spec results and sudo spec ones.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ SimpleCov.start do
   add_filter "/vendor/"
 end
 
+RSpec.configure do |c|
+  if c.filter[:sudo]
+    SimpleCov.command_name 'rspec:sudo'
+  end
+end
+
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'packetgen'
 
