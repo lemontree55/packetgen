b2f37cce7dd3593c7101d59e2d79619e9d7b7bf0
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-11 16:22:34 +0100

    Add skeleton for Header::TCP::Options class

---
+++ lib/packetgen/header/tcp/options.rb
@@ -0,0 +1,14 @@
+module PacketGen
+  module Header
+    class TCP
+
+      # Container for TCP options in {TCP TCP header}.
+      # @author Sylvain Daubert
+      class Options < Array
+
+        def read(str)
+        end
+      end
+    end
+  end
+end
