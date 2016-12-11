d641576473541f9b9ed01b5ac18b1ce6c9d681c2
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-11 16:22:58 +0100

    Header::TCP: complete skeleton

--- lib/packetgen/header/tcp.rb
+++ lib/packetgen/header/tcp.rb
@@ -12,17 +12,93 @@ module PacketGen
       # IP protocol number for TCP
       IP_PROTOCOL = 6
 
+      # @param [Hash] options
+      # @option options [Integer] :sport
+      # @option options [Integer] :dport
+      # @option options [Integer] :seq
+      # @option options [Integer] :ack
+      # @option options [Integer] :hlen
+      # @option options [Integer] :reserved
+      # @option options [Integer] :flags
+      # @option options [Integer] :wsize
+      # @option options [Integer] :sum
+      # @option options [Integer] :urg
+      # @option options [String] :body
       def initialize(options={})
+        super Int16.new(options[:sport]),
+              Int16.new(options[:dport]),
+              Int32.new(options[:seq] || rand(2**32)),
+              Int32.new(options[:ack]),
+              options[:hlen] || 5,
+              options[:reserved] || 0,
+              options[:flags] || 0,
+              Int16.new(options[:wsize]),
+              Int16.new(options[:sum]),
+              Int16.new(options[:urg]),
+              TCP::Options.new,
+              StructFu::String.new.read(options[:body])
       end
 
+      # Read a TCP header from a string
+      # @param [String] str binary string
+      # @return [self]
       def read(str)
+        return self if str.nil?
+        raise ParseError, 'string too short for TCP' if str.size < self.sz
+        force_binary str
+        self[:sport].read str[0, 2]
+        self[:dport].read str[2, 2]
+        self[:seq].read str[4, 4]
+        self[:ack].read str[8, 4]
+        u16 = str[12, 2].unpack 'n'.first
+        self[:hlen] = u16 >> 12
+        self[:reserved] =  (u16 >> 9) & 0x7
+        self[:flags] = u16 & 0x1ff
+        self[:wsize].read str[14, 2]
+        self[:sum].read str[16, 2]
+        self[:urg].read str[18, 2]
+        self[:options].read str[20, (self[:hlen] - 5) * 4] if self[:hlen] > 5
+        self[:body].read str[self[:hlen] * 4..-1]
       end
 
+      # Compute checksum and set +sum+ field
+      # @return [Integer]
       def calc_sum
+        sum = ip_header(self).pseudo_header_sum
+        sum += IP_PROTOCOL
+        sum += self.sz
+        str = self.to_s
+        str << "\x00" if str.length % 2 == 1
+        sum += str.unpack('n*').reduce(:+)
+
+        while sum > 0xffff do
+          sum = (sum & 0xffff) + (sum >> 16)
+        end
+        sum = ~sum & 0xffff
+        self[:sum].value = (sum == 0) ? 0xffff : sum
       end
 
+      # Compute header length and set +hlen+ field
+      # @return [Integer]
       def calc_length
+        self[:hlen] = 5 + self[:options].sz / 4
+      end
+
+      # Get binary string
+      # @return [String]
+      def to_s
+        ary1 = to_a[0..3]
+        ary2 = to_a[7..10]
+        u16 = ((options[:hlen] & 0xf) << 12) |
+              ((options[:reserved] & 0x7) << 9) |
+              (options[:flags] & 0x1ff)
+        ary1.map(&:to_s).join << [u16].pack('n') << ary2.map(&:to_s).join
       end
     end
+
+    IP.bind_header TCP, proto: TCP::IP_PROTOCOL
+    IPv6.bind_header TCP, next: TCP::IP_PROTOCOL
   end
 end
+
+require_relative 'tcp/options'
