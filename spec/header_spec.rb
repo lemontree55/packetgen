5939e0bf503e3d8493b6dfae525f9d5ef27d4aed
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-22 19:51:48 +0100

    sq

--- spec/header_spec.rb
+++ spec/header_spec.rb
@@ -22,7 +22,8 @@ module PacketGen
   describe Header do
     it '.all returns all header classes' do
       expect(Header.all).to eq([Header::Eth, Header::IP, Header::ICMP, Header::ARP,
-                                Header::IPv6, Header::ICMPv6, Header::UDP, Header::TCP])
+                                Header::IPv6, Header::ICMPv6, Header::UDP, Header::TCP,
+                                Header::FakeHeader])
     end
 
     describe '.add_class' do
