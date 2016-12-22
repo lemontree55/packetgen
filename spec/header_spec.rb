f5c211e1ae4dd315d9fc270d83e92e4fcd4bce6b
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-22 19:54:58 +0100

    Simplify 'Packet#add raises on unknown association' spec

--- spec/header_spec.rb
+++ spec/header_spec.rb
@@ -22,8 +22,7 @@ module PacketGen
   describe Header do
     it '.all returns all header classes' do
       expect(Header.all).to eq([Header::Eth, Header::IP, Header::ICMP, Header::ARP,
-                                Header::IPv6, Header::ICMPv6, Header::UDP, Header::TCP,
-                                Header::FakeHeader])
+                                Header::IPv6, Header::ICMPv6, Header::UDP, Header::TCP])
     end
 
     describe '.add_class' do
