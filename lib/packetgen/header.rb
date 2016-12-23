3340ac8ae69782efddbcb2ab6dc712c488f730df
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-23 12:46:07 +0100

    Header: add .add_class and .remove_class

--- lib/packetgen/header.rb
+++ lib/packetgen/header.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file is part of PacketGen
 # See https://github.com/sdaubert/packetgen for more informations
 # Copyright (C) 2016 Sylvain Daubert <sylvain.daubert@laposte.net>
@@ -8,11 +9,37 @@ module PacketGen
   # @author Sylvain Daubert
   module Header
 
+    @added_header_classes = {}
+
     # Get known header classes
     # @return [Array<Class>]
     def self.all
-      constants.map { |sym| const_get sym }.
-        select { |klass| klass < Struct && klass < HeaderMethods }
+      return @header_classes if @header_classes
+
+      @builtin ||= constants.map { |sym| const_get sym }.
+                 select { |klass| klass < Struct && klass < HeaderMethods }
+      @header_classes = @builtin + @added_header_classes.values
+    end
+
+    # Add a foreign header class to known header classes. This is
+    # needed by {Packet.gen} and {Packet#add}.
+    # @param [Class] klass a header class, which should include
+    #   {Header::HeaderMethods} and {Header::HeaderClassMethods}
+    # @return [void]
+    def self.add_class(klass)
+      protocol_name = klass.to_s.sub(/.*::/, '')
+      @added_header_classes[protocol_name] = klass
+      @header_classes = nil
+    end
+
+    # Remove a foreign header (previously added by {.add_header_class}à
+    # from known header classes.
+    # @param [Class] klass
+    # @return [void]
+    def self.remove_class(klass)
+      protocol_name = klass.to_s.sub(/.*::/, '')
+      @added_header_classes.delete protocol_name
+      @header_classes = nil
     end
   end
 end
