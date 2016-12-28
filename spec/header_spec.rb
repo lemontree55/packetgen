3e8c55613d95ad8858569c137e17aca5aaeb208b
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-12-28 12:43:48 +0100

    Fix a header spec to run with ruby 2.4

--- spec/header_spec.rb
+++ spec/header_spec.rb
@@ -21,8 +21,9 @@ module PacketGen
 
   describe Header do
     it '.all returns all header classes' do
-      expect(Header.all).to eq([Header::Eth, Header::IP, Header::ICMP, Header::ARP,
-                                Header::IPv6, Header::ICMPv6, Header::UDP, Header::TCP])
+      expect(Header.all).to include(Header::Eth, Header::IP, Header::ICMP, Header::ARP,
+                                    Header::IPv6, Header::ICMPv6, Header::UDP,
+                                    Header::TCP)
     end
 
     describe '.add_class' do
