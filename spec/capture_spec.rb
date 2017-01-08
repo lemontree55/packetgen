855c1a7a7e3b7f809df653800d12f5e7ad9f235d
Kent Gruber <kgruber1@emich.edu>
2017-01-08 15:47:21 -0500

    Remove required iface method argument.
    Moved the required first argument for the capture method for packet
    capturing into an optional option that uses Pcaprub’s default interface
    lookup if nothing is set by the user.

--- spec/capture_spec.rb
+++ spec/capture_spec.rb
@@ -3,24 +3,24 @@ require_relative 'spec_helper'
 module PacketGen
   describe Capture do
 
-    let(:iface) { PacketGen.default_iface }
+    let(:iface) { Pcap.lookupdev }
 
     describe '#initialize' do
       it 'accepts no options' do
         cap = nil
-        expect { cap = Capture.new(iface) }.to_not raise_error
+        expect { cap = Capture.new }.to_not raise_error
         expect(cap).to be_a(Capture)
       end
 
       it 'accepts options' do
         options = { max: 12, timeout: 30, filter: 'ip', promisc: true, snaplen: 45 }
-        expect { Capture.new(iface, options) }.to_not raise_error
+        expect { Capture.new(options) }.to_not raise_error
       end
     end
 
     describe '#start', :sudo do
       it 'capture packets and returns a array of Packet' do
-        cap = capture('lo') do
+        cap = capture(iface: 'lo') do
           system 'ping 127.0.0.1 -c 3 -i 0.2 > /dev/null'
         end
 
@@ -34,7 +34,7 @@ module PacketGen
       end
 
       it 'capture packets until :timeout seconds' do
-        cap = Capture.new('lo')
+        cap = Capture.new(iface: 'lo')
         before = Time.now
         cap.start(timeout: 1)
         after = Time.now
@@ -42,18 +42,20 @@ module PacketGen
       end
 
       it 'capture packets using a filter' do
-        cap = capture('lo', filter: 'ip dst 127.0.0.2') do
+        cap = capture(iface: 'lo', filter: 'ip dst 127.0.0.2') do
+          #system 'ping -c 1 127.0.0.1 > /dev/null'
           system '(ping -c 1 127.0.0.1; ping -c 1 127.0.0.2) > /dev/null'
         end
 
         packets = cap.packets
+        binding.pry
         expect(packets.size).to eq(1)
         expect(packets.first.ip.src).to eq('127.0.0.1')
         expect(packets.first.ip.dst).to eq('127.0.0.2')
       end
 
       it 'capture raw packets with option parse: false' do
-        cap = capture('lo', parse: false) do
+        cap = capture(iface: 'lo', parse: false) do
           system 'ping 127.0.0.1 -c 1 > /dev/null'
         end
 
@@ -63,7 +65,7 @@ module PacketGen
       end
 
       it 'capture :max packets' do
-        cap = capture('lo', max: 2) do
+        cap = capture(iface: 'lo', max: 2) do
           system 'ping -c 2 -i 0.2 127.0.0.1 > /dev/null'
         end
 
@@ -73,7 +75,7 @@ module PacketGen
 
       it 'yields captured packets' do
         yielded_packets = []
-        cap = Capture.new('lo', timeout: 3)
+        cap = Capture.new(iface: 'lo', timeout: 3)
         cap_thread = Thread.new { cap.start { |pkt| yielded_packets << pkt } }
         sleep 0.1
         system 'ping -c 2 127.0.0.1 > /dev/null'
@@ -85,7 +87,7 @@ module PacketGen
 
       it 'yields captured raw packets' do
         yielded_packets = []
-        cap = Capture.new('lo', parse: false, timeout: 3)
+        cap = Capture.new(iface: 'lo', parse: false, timeout: 3)
         cap_thread = Thread.new { cap.start { |pkt| yielded_packets << pkt } }
         sleep 0.1
         system 'ping -c 2 127.0.0.1 > /dev/null'
