41228f8dff601202f0abf06b2af1aad14f2555eb
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-03-13 19:55:33 +0100

    Header::IKE: add void specs

--- spec/header/ike_spec.rb
+++ spec/header/ike_spec.rb
@@ -15,6 +15,31 @@ module PacketGen
           expect(UDP).to know_header(IKE).with(sport: 4500)
         end
       end
+
+      describe '#initialize' do
+        it 'creates a IKE header with default values'
+        it 'accepts options'
+      end
+
+      describe '#read' do
+        let(:ike) { IKE.new }
+
+        it 'sets header from a string'
+        it 'also sets ICV when ICV length was previously set'
+      end
+
+      describe '#to_s' do
+        it 'returns a binary string'
+      end
+
+      describe '#inspect' do
+        it 'returns a String with all attributes'
+      end
+
+      context '(parsing)' do
+        it 'is parsed when first 32-bit word in UDP body is null'
+        it 'is not parsed when first 32-bit word in UDP body is not null'
+      end
     end
   end
 end
