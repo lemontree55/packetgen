fda378dd82128a4a171f7c80b69cca8315dd2a07
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-03-24 14:53:09 +0100

    Types::Array: remove counter management from DNS::RRsection and implement it in Array. From now, Types::Array#push and Types::Array#<< have different semantics when initialized with a counter.

--- spec/types/array_spec.rb
+++ spec/types/array_spec.rb
@@ -14,16 +14,17 @@ module PacketGen
       end
       class BadClass < Array; end
       
+      let(:tlv) { TLV.new }
+
       context '#push' do
-        let(:tlv) { TLV.new }
         let(:g) { GoodClass.new }
         let(:g2) { GoodClass.new }
         let(:b) { BadClass.new }
 
         it 'accepts an object' do
-          expect { g << tlv }.to change { g.size }.by(1)
-          expect { g2 << tlv }.to change { g2.size }.by(1)
-          expect { b << tlv }.to change { b.size }.by(1)
+          expect { g.push tlv }.to change { g.size }.by(1)
+          expect { g2.push tlv }.to change { g2.size }.by(1)
+          expect { b.push tlv }.to change { b.size }.by(1)
         end
 
         it 'accepts a Hash when .set_of is used' do
@@ -44,6 +45,29 @@ module PacketGen
 
         it 'raises when a Hash is passed and .set_of is used nor #record_from_hash is redefined' do
           expect { b << { type: 1, value: '43' } }.to raise_error(NotImplementedError)
+        end
+
+        it 'does not update counter if one was declared at initialization' do
+          int32 = Int32.new
+          ary = Array.new(counter: int32)
+          expect { ary.push tlv }.to_not change { int32.to_i }
+        end
+      end
+
+      context '#<<' do
+        it 'updates counter if one was declared at initialization' do
+          int32 = Int32.new
+          ary = Array.new(counter: int32)
+          expect { ary << tlv }.to change { int32.to_i }.by(1)
+        end
+      end
+
+      context '#delete' do
+        it 'updates counter if one was declared at initialization' do
+          int32 = Int32.new
+          ary = Array.new(counter: int32)
+          ary << tlv
+          expect { ary.delete tlv }.to change { int32.to_i }.by(-1)
         end
       end
     end
