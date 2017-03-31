0c395f921c842624c369d6251a5dfb5a1caa1dc7
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-03-31 10:05:45 +0200

    Header::IKE::Notify: fix doc

--- lib/packetgen/header/ike/notify.rb
+++ lib/packetgen/header/ike/notify.rb
@@ -6,7 +6,7 @@ module PacketGen
       # This class handles Notify payloads, as defined in RFC 7296 §3.10.
       #
       # A Notify payload contains a generic payload header (see {Payload}) and
-      # datasom specific fields:
+      # some specific fields:
       #                        1                   2                   3
       #    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
       #   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
