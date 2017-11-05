2c45e56dde640e54bc27b4c63d2e48d93385519c
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-11-05 14:48:33 +0100

    Add Header::EAP protocol

---
+++ lib/packetgen/header/eap.rb
@@ -0,0 +1,134 @@
+# This file is part of PacketGen
+# See https://github.com/sdaubert/packetgen for more informations
+# Copyright (C) 2016 Sylvain Daubert <sylvain.daubert@laposte.net>
+# This program is published under MIT license.
+
+module PacketGen
+  module Header
+
+    # Extensible Authentication Protocol (EAP),
+    # {https://tools.ietf.org/html/rfc3748 RFC 3748}
+    # @author Sylvain Daubert
+    class EAP < Base
+
+      # EAP known codes
+      CODES = {
+        'Request'   => 1,
+        'Response'  => 2,
+        'Success'   => 3,
+        'Failure'   => 4
+      }
+      
+      # EAP known request/response types
+      TYPES = {
+        'Identity'           => 1,
+        'Notification'       => 2,
+        'Nak'                => 3,
+        'MD5-Challenge'      => 4,
+        'One Time Password'  => 5,
+        'Generic Token Card' => 6,
+        'Expanded Types'     => 254,
+        'Experimental Use'   => 255
+      }
+
+      # @!attribute code
+      #  @return [Integer] 8-bit EAP code
+      define_field :code, Types::Int8Enum, enum: CODES
+
+      # @!attribute id
+      #  @return [Integer] 8-bit identifier
+      define_field :id, Types::Int8
+
+      # @!attribute length
+      #  @return [Integer] 16-bit length of EAP packet
+      define_field :length, Types::Int16, default: 4
+      
+      # @!attribute type
+      #  This field is present only for Request or Response packets,
+      #  with type different from Expanded Types (254).
+      #  @return [Integer] 8-bit request or response type
+      define_field :type, Types::Int8Enum, enum: TYPES, 
+                   optional: ->(eap) { [1, 2].include? eap.code }
+                   
+      # @!attribute vendor_id
+      #  This field is present only for Request or Response packets,
+      #  with type equal to +Expanded Types+ (254).
+      #  @return [Integer] 24-bit vendor ID
+      define_field :vendor_id, Types::Int24,
+                   optional: ->(eap) { [1, 2].include?(eap.code) and eap.type == 254 }
+      
+      # @!attribute vendor_type
+      #  This field is present only for Request or Response packets,
+      #  with type equal to +Expanded Types+ (254).
+      #  @return [Integer] 32-bit vendor type
+      define_field :vendor_type, Types::Int32,
+                   optional: ->(eap) { [1, 2].include?(eap.code) and eap.type == 254 }
+
+      # @!attribute body
+      #  @return [Types::String, Header::Base]
+      define_field :body, Types::String
+      
+      # @return [EAP]
+      def initialize(options={})
+        super
+        calc_length if options[:length].nil?
+      end
+
+      # Get human readable code
+      # @return [String]
+      def human_code
+        self[:code].to_human
+      end
+      
+      # Get human readable type
+      # @return [String]
+      # @raise [ParseError] not a Request nor a Response packet
+      def human_type
+        raise ParseError, 'not a Request nor a Response' unless [1,2].include?(code)
+        self[:type].to_human
+      end
+      
+      # Is packet a request?
+      # @return [Boolean]
+      def request?
+        code == CODES['Request']
+      end
+      
+      # Is packet a response?
+      # @return [Boolean]
+      def response?
+        code == CODES['Response']
+      end
+      
+      # Is packet a success?
+      # @return [Boolean]
+      def success?
+        code == CODES['Success']
+      end
+      
+      # Is packet a failure?
+      # @return [Boolean]
+      def failure?
+        code == CODES['Failure']
+      end
+      
+      # Return an array of desired authentication types from a Nak packet
+      # @return [Array<Integer>]
+      # @raise [ParseError] not a Nak packet
+      def desired_auth_type
+        if code != 2 and type != 3
+          raise ParseError, 'not a Nak response'
+        end
+        body.to_s.unpack('C*')
+      end
+      
+      # Calculate length field from content
+      # @return [Integer]
+      def calc_length
+        self.length = sz
+      end
+    end
+    
+    Dot1x.bind_header EAP, type: 0
+  end
+end
---
+++ spec/header/eap_spec.rb
@@ -0,0 +1,153 @@
+require_relative '../spec_helper'
+
+module PacketGen
+  module Header
+    describe EAP do
+      describe 'binding' do
+        it 'in Dot1x packets' do
+          expect(Dot1x).to know_header(EAP).with(type: 0)
+        end
+      end
+      
+      describe '#initialize' do
+        it 'creates a EAP header with default values' do
+          eap = EAP.new
+          expect(eap).to be_a(EAP)
+          expect(eap.code).to eq(1)
+          expect(eap.id).to eq(0)
+          expect(eap.length).to eq(5)
+          expect(eap.is_present?(:type)).to be(true)
+          expect(eap.type).to eq(1)
+          expect(eap.body).to eq('')
+        end
+
+        it 'accepts options' do
+          options = {
+            code: 0xf5,
+            id: 0x81,
+            length: 1000,
+            body: 'this is a body'
+          }
+          eap = EAP.new(options)
+          options.each do |key, value|
+            expect(eap.send(key)).to eq(value)
+          end
+        end
+        
+        %w(Success Failure).each do |code|
+          it "does not generate a type field for #{code} code" do
+            eap = EAP.new(code: code)
+            expect(eap.is_present?(:type)).to be(false)
+          end
+        end
+      end
+
+      describe '#read' do
+        let(:eap) { EAP.new }
+
+        it 'sets header from a string' do
+          str = (1..eap.sz).to_a.pack('C*') + 'body'
+          eap.read str
+          expect(eap.code).to eq(1)
+          expect(eap.id).to eq(2)
+          expect(eap.length).to eq(0x0304)
+          expect(eap.type).to eq(5)
+          expect(eap.body).to eq('body')
+          
+          str = (3..(EAP.new.sz+2)).to_a.pack('C*') + 'body'
+          eap.read str
+          expect(eap.code).to eq(3)
+          expect(eap.id).to eq(4)
+          expect(eap.length).to eq(0x0506)
+          expect(eap.body).to eq("\x07body")
+        end
+        
+        it 'decodes a complex string' do
+          Dot11.has_fcs = false
+          file = File.join(__dir__, 'wps.pcapng')
+          raws = PcapNG::File.new.read_packet_bytes(file)
+          
+          pkt = Packet.parse(raws[2])
+          expect(pkt.is? 'Dot11::Data').to be(true)
+          expect(pkt.is? 'Dot1x').to be(true)
+          expect(pkt.is? 'EAP').to be(true)
+          expect(pkt.eap).to be_a_response
+          expect(pkt.eap.code).to eq(2)
+          expect(pkt.eap.human_code).to eq('Response')
+          expect(pkt.eap.id).to eq(13)
+          expect(pkt.eap.type).to eq(1)
+          expect(pkt.eap.human_type).to eq('Identity')
+          expect(pkt.body).to eq('WFA-SimpleConfig-Enrollee-1-0')
+
+          pkt = Packet.parse(raws[3])
+          expect(pkt.is? 'EAP').to be(true)
+          expect(pkt.eap).to be_a_request
+          expect(pkt.eap.code).to eq(1)
+          expect(pkt.eap.human_code).to eq('Request')
+          expect(pkt.eap.id).to eq(14)
+          expect(pkt.eap.type).to eq(254)
+          expect(pkt.eap.human_type).to eq('Expanded Types')
+          expect(pkt.eap.vendor_id).to eq(0x372a)
+          expect(pkt.eap.vendor_type).to eq(1)
+          expect(pkt.body).to eq(PacketGen.force_binary("\x01\x00"))
+          
+          pkt = Packet.parse(raws.last)
+          expect(pkt.is? 'EAP').to be(true)
+          expect(pkt.eap).to be_a_failure
+          expect(pkt.eap).to_not be_a_success
+          expect(pkt.eap.human_code).to eq('Failure')
+          expect(pkt.eap.id).to eq(18)
+        end
+      end
+      
+      describe '#to_s' do
+        it 'returns a binary string without type field' do
+          eap = EAP.new(code: 'Success')
+          expect(eap.to_s).to eq(PacketGen.force_binary("\x03\x00\x00\x04"))
+        end
+
+        it 'returns a binary string with type field' do
+          eap = EAP.new(code: 'Request', type: 46)
+          expect(eap.to_s).to eq(PacketGen.force_binary("\x01\x00\x00\x05\x2e"))
+        end
+
+        it 'returns a binary string with type and vendor fields' do
+          eap = EAP.new(code: 'Request', type: 254)
+          expect(eap.to_s).to eq(PacketGen.force_binary("\x01\x00\x00\x0c\xfe\x00\x00\x00\x00\x00\x00\x00"))
+        end
+      end
+      
+      describe '#inspect' do
+        it 'returns a string without type field' do
+          eap = EAP.new(code: 'Success')
+          str = eap.inspect
+          %i(code id length).each do |attr|
+            expect(str).to include(attr.to_s)
+          end
+          %i(type vendor_id vendor_type).each do |attr|
+            expect(str).to_not include(attr.to_s)
+          end
+        end
+
+        it 'returns a string with type field' do
+          eap = EAP.new(code: 'Request', type: 46)
+          str = eap.inspect
+          %i(code id length type).each do |attr|
+            expect(str).to include(attr.to_s)
+          end
+          %i(vendor_id vendor_type).each do |attr|
+            expect(str).to_not include(attr.to_s)
+          end
+        end
+
+        it 'returns a string with type and vendor fields' do
+          eap = EAP.new(code: 'Request', type: 254)
+          str = eap.inspect
+          (eap.fields - %i(body)).each do |attr|
+            expect(str).to include(attr.to_s)
+          end
+        end
+      end
+    end
+  end
+end
