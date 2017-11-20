2dccbabfe1eb7e0211c12f66db1f2e2145a4cc36
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-11-20 20:08:22 +0100

    Add DHCP options

--- lib/packetgen/header/dhcp.rb
+++ lib/packetgen/header/dhcp.rb
@@ -9,12 +9,17 @@ module PacketGen
     # Dynamic Host Configuration Protocol, {https://tools.ietf.org/html/rfc2131 
     # RFC 2131}
     # @author Sylvain Daubert
+    class DHCP < Base; end
+
+    require_relative 'dhcp/option'
+    require_relative 'dhcp/options'
+
     class DHCP < Base
       # DHCP magic value in BOOTP options
       DHCP_MAGIC = 0x63825363
 
       define_field :magic, Types::Int32, default: 0x63825563
-      define_field :options, Types::String
+      define_field :options, DHCP::Options
       
       # differentiate from BOOTP by checking presence of DHCP magic
       # @return [Boolean]
