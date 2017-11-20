6297d04c0ee1a55c1569f28987bf9762a5a6814d
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-11-20 20:08:46 +0100

    Add Header::BOOTP and Header::DHCP specs

--- spec/header/bootp_spec.rb
+++ spec/header/bootp_spec.rb
@@ -17,8 +17,32 @@ module PacketGen
         it 'read a BOOTP header' do
           raw = PcapNG::File.new.read_packet_bytes(File.join(__dir__, 'dhcp.pcapng')).first
           pkt = PacketGen.parse(raw)
-          p pkt
           expect(pkt.is? 'BOOTP').to be(true)
+          bootp = pkt.bootp
+          expect(bootp.op.to_human).to eq('BOOTREQUEST')
+          expect(bootp.htype).to eq(1)
+          expect(bootp.hlen).to eq(6)
+          expect(bootp.hops).to eq(0)
+          expect(bootp.xid).to eq(15645)
+          expect(bootp.secs).to eq(0)
+          expect(bootp.flags).to eq(0)
+          expect(bootp.ciaddr.to_human).to eq('0.0.0.0')
+          expect(bootp.yiaddr.to_human).to eq('0.0.0.0')
+          expect(bootp.siaddr.to_human).to eq('0.0.0.0')
+          expect(bootp.giaddr.to_human).to eq('0.0.0.0')
+          expect(bootp.chaddr.to_human).to eq('00:0b:82:01:fc:42')
+          expect(bootp.sname).to eq('')
+          expect(bootp.file).to eq('')
+        end
+      end
+
+      describe '#to_s' do
+        it 'returns a binary string' do
+          raws = PcapNG::File.new.read_packet_bytes(File.join(__dir__, 'dhcp.pcapng'))
+          packets = PcapNG::File.new.read_packets(File.join(__dir__, 'dhcp.pcapng'))
+          packets.each_with_index do |pkt, i|
+            expect(pkt.to_s).to eq(raws[i])
+          end
         end
       end
     end
--- spec/header/dhcp_spec.rb
+++ spec/header/dhcp_spec.rb
@@ -11,6 +11,25 @@ module PacketGen
           expect(BOOTP).to know_header(DHCP)
         end
       end
+
+      describe '#read' do
+        it 'read a DHCP header' do
+          raw = PcapNG::File.new.read_packet_bytes(File.join(__dir__, 'dhcp.pcapng')).first
+          pkt = PacketGen.parse(raw)
+          expect(pkt.is? 'BOOTP').to be(true)
+          expect(pkt.is? 'DHCP').to be(true)
+
+          dhcp = pkt.dhcp
+          expect(dhcp.magic).to eq(0x63825363)
+          expect(dhcp.options.size).to eq(8)
+          expect(dhcp.options.first.human_type).to eq('message-type')
+          expect(dhcp.options.first.length).to eq(1)
+          expect(dhcp.options.first.value).to eq(1)
+          expect(dhcp.options.last.human_type).to eq('pad')
+          expect(dhcp.options.last.length).to eq(0)
+          expect(dhcp.options.last.value).to eq('')
+        end
+      end
     end
   end
 end
