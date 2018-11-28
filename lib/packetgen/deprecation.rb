c4e0debcd319a29defbc3dc96235b28a593e7691
Sylvain Daubert <sylvain.daubert@laposte.net>
2018-11-28 15:01:07 +0100

    Add Deprecation.deprecated_class method.

--- lib/packetgen/deprecation.rb
+++ lib/packetgen/deprecation.rb
@@ -4,7 +4,19 @@ module PacketGen
   # @author Sylvain Daubert
   # @api private
   module Deprecation
-    def self.deprecated(klass, deprecated_method, new_method=nil, klass_method: false, remove_version: '4.0.0')
+    # Default remove version for deprecated classes/methods
+    # @since 3.1.0
+    REMOVE_VERSION = '4.0.0'
+
+    # Warn when using a deprecated method
+    # @param [Module] klass class/module of deprecated method
+    # @param [Symbol,String] deprecated_method
+    # @param [Symbol,String,nil] new_method method to use instead of deprecated one
+    # @param [Boolean] klass_method +deprecated_method+ is a class method (+true+)
+    #                  or a, instance one (+false+)
+    # @param [String] remove_version version from which +deprecated_method+ will
+    #                 no more exist.
+    def self.deprecated(klass, deprecated_method, new_method=nil, klass_method: false, remove_version: REMOVE_VERSION)
       separator = klass_method ? '.' : '#'
       base_name = klass.to_s + separator
       complete_deprecated_method_name = base_name + deprecated_method.to_s
@@ -12,7 +24,20 @@ module PacketGen
 
       file, line = caller(2).first.split(':')[0, 2]
       message = +"#{file}:#{line}: #{complete_deprecated_method_name} is deprecated"
-      message << "in favor of #{complete_new_method_name}" unless new_method.nil?
+      message << " in favor of #{complete_new_method_name}" unless new_method.nil?
+      message << ". It will be remove in PacketGen #{remove_version}."
+      warn message
+    end
+
+    # Warn when using a deprecated method
+    # @param [Module] klass deprecated class/module
+    # @param [Module] new_klass class/module to use instead of +klass+
+    # @param [String] remove_version version from which +klass+ will
+    #                 no more exist.
+    def self.deprecated_class(klass, new_klass=nil, remove_version: REMOVE_VERSION)
+      file, line = caller(2..2).first.split(':')[0, 2]
+      message = +"#{file}:#{line}: #{klass} is deprecated"
+      message << " in favor od #{new_klass}" unless new_klass.nil?
       message << ". It will be remove in PacketGen #{remove_version}."
       warn message
     end
