4c5b20105ce9fd3c609b98787aa7044f05c57030
Sylvain Daubert <sylvain.daubert@laposte.net>
2019-01-04 18:13:04 +0100

    Fix copyright notice.

--- lib/packetgen/types/enum.rb
+++ lib/packetgen/types/enum.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 # This file is part of PacketGen
 # See https://github.com/sdaubert/packetgen for more informations
-# Copyright (C) 2032 Sylvain Daubert <sylvain.daubert@laposte.net>
+# Copyright (C) 2016 Sylvain Daubert <sylvain.daubert@laposte.net>
 # This program is published under MIT license.
 
 # frozen_string_literal: true
