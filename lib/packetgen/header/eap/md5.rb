0fea20d31ee48caf34cae355202066d332f5c589
lemontree55 <lenontree@proton.me>
2025-02-23 17:33:07 +0100

    Rename EAP::MD5#optional_name to #name
    Create an alias #optional_name to #name. Deprecate this alias.

--- lib/packetgen/header/eap/md5.rb
+++ lib/packetgen/header/eap/md5.rb
@@ -18,15 +18,23 @@ module PacketGen
         remove_attr :body
 
         # @!attribute value_size
-        #  @return [Integer] 8-bit value size
+        #  8-bit size of the {#value} attribute.
+        #  @return [Integer]
         define_attr :value_size, BinStruct::Int8
         # @!attribute value
+        #  MD5 challenge value, as an octet stream, as per {https://datatracker.ietf.org/doc/html/rfc1994#section-4 RFC1994}
         #  @return [::String]
         define_attr :value, BinStruct::String,
                     builder: ->(h, t) { t.new(length_from: h[:value_size]) }
-        # @!attribute optional_name
+        # @!attribute name
+        #  Name identifying the system sending the packet. It is or or more octets. Its size is
+        #  determined from the {#length} attribute.
         #  @return [::String]
-        define_attr :optional_name, BinStruct::String
+        # @since 4.1.0 Attribute renamed from +optioanl_name+ to +name+
+        define_attr :name, BinStruct::String
+
+        # @deprecated
+        alias optional_name name
       end
     end
   end
